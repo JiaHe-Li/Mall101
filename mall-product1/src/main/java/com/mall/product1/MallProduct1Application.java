975faf59a1cf7580087832c21c5848e37122dd2f
li_jiahe <lijiahe888888@gmail.com>
2022-09-04 14:57:13 +0800

    项目初始结构创建

---
+++ mall-product1/src/main/java/com/mall/product1/MallProduct1Application.java
@@ -0,0 +1,13 @@
+package com.mall.product1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallProduct1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallProduct1Application.class, args);
+    }
+
+}
---
+++ mall-product1/src/test/java/com/mall/product1/MallProduct1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.product1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallProduct1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
